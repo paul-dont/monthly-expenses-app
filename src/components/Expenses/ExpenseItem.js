28018be034fa43042449d1d349e5f23648f3d2bc
Pavlos <pauldontas@gmail.com>
2022-05-09 20:08:32 +0300

    fixed a bug with title prop

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,14 +4,12 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-
   return (
     <li>
       <Card className="expense-item">
         <ExpenseDate date={props.date} />
         <div className="expense-item__description">
-          <h2>{title}</h2>
+          <h2>{props.title}</h2>
           <div className="expense-item__price">${props.amount}</div>
         </div>
       </Card>
